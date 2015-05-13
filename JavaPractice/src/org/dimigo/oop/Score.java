26d425804e1bfc47ba40a6f7f67f7ba0f27c97d1
허석민 <gjtjrals0@naver.com>
2015-05-13 15:05:09 +0900

    실습과제 9

--- JavaPractice/src/org/dimigo/oop/Score.java
+++ JavaPractice/src/org/dimigo/oop/Score.java
@@ -30,17 +30,17 @@ public class Score {
 		
 		System.out.println();
 		
-		StringBuilder sb = new StringBuilder();
-		sb.append("<< 점수 출력 >>\n")
+		String sb = new StringBuilder()
+		  .append("<< 점수 출력 >>\n")
 		  .append("국어 점수:").append(kor).append("점\n")
 		  .append("수학 점수:").append(mat).append("점\n")
 		  .append("영어 점수:").append(eng).append("점\n")
 		  .append("총점:").append(kor+mat+eng).append("점\n")
-		  .append("평균:").append(String.format("%.1f",(kor+mat+eng)/3.0)).append("점\n");
+		  .append("평균:").append(String.format("%.1f",(kor+mat+eng)/3.0)).append("점\n").toString();
 		
-		System.out.println(sb.toString());
+		System.out.println(sb);
 		
 		sc.close();
 	}
-
+	
 }
