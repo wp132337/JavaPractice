244fe5fa2d29081597e57f3b22de0675320d77d2
seokmin <seokmin@SeokMin>
2015-05-27 14:35:21 +0900

    실습과제 11

---
+++ JavaPractice/src/org/dimigo/oop/IdolGroup.java
@@ -0,0 +1,35 @@
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *	 |_ IdolGroup
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2015. 5. 27.
+ * </pre>
+ *
+ * @author	: 허석민
+ * @verson	: 1.0
+ */
+public class IdolGroup {
+	
+	public static void main(String[] args) {
+		String[] idolGroup = {"빅뱅", "2NE1", "걸스데이"};
+		String[][] members = {
+				{"GD","태양","대성","탑","승리"},
+				{"cL","산다라박","박봄","민지"},
+				{"유라","소진","민아","혜리","그외"}
+		};
+		
+		for(int i=0; i<3; i++) {
+			System.out.println("<< " + idolGroup[i] + " 멤버 >>");
+			
+			for(int j=0; j<members[i].length; j++)
+				System.out.println(members[i][j]);
+			
+			System.out.println();
+		}
+	}
+
+}
