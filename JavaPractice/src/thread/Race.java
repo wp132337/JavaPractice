033abf1a19ee3da51cbb9f8d56c0c10accb98753
허석민 <gjtjrals0@naver.com>
2015-11-06 14:05:36 +0900

    실습과제 9

--- JavaPractice/src/thread/Race.java
+++ JavaPractice/src/thread/Race.java
@@ -23,6 +23,9 @@ public class Race {
 		Thread t1 = new Runner("홍길동");
 		Thread t2 = new Runner("홍길순");
 		
+		t1.setPriority(Thread.MAX_PRIORITY);
+		t2.setPriority(Thread.MIN_PRIORITY);
+		
 		t1.start();
 		t2.start();
 		
